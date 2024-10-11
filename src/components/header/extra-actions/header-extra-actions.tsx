e862bd941a29f77c8225310a15a56fdc5774fdf5
Joel Campos <joelcamposoliva9000@gmail.com>
2024-10-11 20:05:26 +0200

    feat: Add rounded actions box

--- src/components/header/extra-actions/header-extra-actions.tsx
+++ src/components/header/extra-actions/header-extra-actions.tsx
@@ -3,11 +3,11 @@ import { SupervisedUserCircle, ShoppingCart } from "@mui/icons-material";
 import styles from "./header-extra-actions.module.css";
 import clsx from "clsx";
 
-const linkClassName = clsx(styles.action, "p-2");
+const linkClassName = clsx(styles.action, "p-2 rounded-md");
 
 export default function HeaderExtraActions() {
   return (
-    <div className="h-14 max-w-24 flex items-center content-center">
+    <div className="h-14 max-w-24 gap-1 flex items-center content-center">
       <Link href="/" className={linkClassName}>
         <ShoppingCart />
       </Link>
